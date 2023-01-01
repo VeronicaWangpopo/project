5d6f49c946f026b4259f1a78611e26e3fcdc931a
veronica_wang <veronica@shoplineapp.com>
2023-01-01 23:38:12 +0800

    update

--- Ticket/script.js
+++ Ticket/script.js
@@ -10,33 +10,76 @@ function openMyOrder() {
   );
 }
 
+$(function () {
+  $.datepicker.regional["it"] = {
+    closeText: "關閉", // set a close button text
+    currentText: "今天", // set today text
+    monthNames: [
+      "一月",
+      "二月",
+      "三月",
+      "四月",
+      "五月",
+      "六月",
+      "七月",
+      "八月",
+      "九月",
+      "十月",
+      "十一月",
+      "十二月",
+    ],
+    dayNamesMin: ["日", "一", "二", "三", "四", "五", "六"],
+    dateFormat: "yy/mm/dd",
+    defaultDate: new Date(),
+  };
+  $.datepicker.setDefaults($.datepicker.regional["it"]);
+  $("#datepicker").datepicker();
+  $("#datepicker").datepicker("setDate", "+10d");
+});
+
 function run() {
-  const dateInput = $("#dateInput");
+  const dateInput = $("#datepicker");
   const timeSelect = $("#timeSelect");
+  const placeSelect = $("#placeSelect");
   const date = dateInput.val();
   const time = timeSelect.val();
+  const place = placeSelect.val();
+  let error = false;
 
   dateInput.removeClass("is-invalid");
   timeSelect.removeClass("is-invalid");
   // 檢查是否輸入
-  if (!date & !time) {
+  if (!date & (time.length === 0) & (place.length === 0)) {
     dateInput.addClass("is-invalid");
     timeSelect.addClass("is-invalid");
+    placeSelect.addClass("is-invalid");
     return;
   }
   if (!date) {
     dateInput.addClass("is-invalid");
-    return;
-  }
-  if (!time) {
-    timeSelect.addClass("is-invalid");
-    return;
+    error = true;
   }
 
-  const urls = time.map(
-    (t) =>
-      `https://www.ymca.com.tw/xwt88.aspx?module=net_booking&files=booking_place&StepFlag=25&QPid=88&QTime=${t}&PT=1&D=${date}`
-  );
+  if (time.length === 0) {
+    timeSelect.addClass("is-invalid");
+    error = true;
+  }
+
+  if (place.length === 0) {
+    placeSelect.addClass("is-invalid");
+    error = true;
+  }
+
+  if (error) return;
+
+  let urls = time
+    .map((t) =>
+      place.map(
+        (p) =>
+          `https://www.ymca.com.tw/xwt88.aspx?module=net_booking&files=booking_place&StepFlag=25&QPid=${p}&QTime=${t}&PT=1&D=${date}`
+      )
+    )
+    .flat();
 
   $("#active").addClass("disabled");
   $("#activeTitle").removeClass("d-none");

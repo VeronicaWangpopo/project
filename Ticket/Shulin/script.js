3d79f68bd5290bf6c3784873f3633679b14d7f69
veronica_wang <veronica@shoplineapp.com>
2023-07-26 20:52:06 +0800

    新增樹林區訂票系統

---
+++ Ticket/Shulin/script.js
@@ -0,0 +1,123 @@
+setInterval(() => {
+  const now = new Date();
+  const time = `${now.getHours()}：${now.getMinutes()}：${now.getSeconds()}：${now.getMilliseconds()}`;
+  $("#nowTime").text(time);
+}, 100);
+
+function openMyOrderXuanen() {
+  window.open(
+    "https://bnt.xuanen.com.tw/nt01.aspx?module=member&files=orderx_mt"
+  );
+}
+
+$(function () {
+  // Date
+  $.datepicker.regional["it"] = {
+    closeText: "關閉", // set a close button text
+    currentText: "今天", // set today text
+    monthNames: [
+      "一月",
+      "二月",
+      "三月",
+      "四月",
+      "五月",
+      "六月",
+      "七月",
+      "八月",
+      "九月",
+      "十月",
+      "十一月",
+      "十二月",
+    ],
+    dayNamesMin: ["日", "一", "二", "三", "四", "五", "六"],
+    dateFormat: "yy/mm/dd",
+    defaultDate: new Date(),
+  };
+  $.datepicker.setDefaults($.datepicker.regional["it"]);
+  $("#datepicker").datepicker();
+  $("#datepicker").datepicker("setDate", "+7d");
+
+  // Local
+  const localSelect = $("#localSelect");
+  localSelect.change(() => {
+    const placeSelect = $("#placeSelect");
+    placeSelect.html(`
+      <option value="83" selected>場地一</option>
+      <option value="84">場地二</option>
+      <option value="85">場地三</option>
+      <option value="86">場地四</option>
+      <option value="87">場地五</option>
+      <option value="88" selected>場地六</option>`);
+  });
+});
+
+function run() {
+  const dateInput = $("#datepicker");
+  const timeSelect = $("#timeSelect");
+  const placeSelect = $("#placeSelect");
+  const localSelect = $("#localSelect");
+  const date = dateInput.val();
+  const time = timeSelect.val();
+  const place = placeSelect.val();
+  const local = localSelect.val();
+  const domain = "https://bnt.xuanen.com.tw/nt01.aspx";
+
+  let error = false;
+
+  dateInput.removeClass("is-invalid");
+  timeSelect.removeClass("is-invalid");
+  // 檢查是否輸入
+  if (!date & (time.length === 0) & (place.length === 0)) {
+    dateInput.addClass("is-invalid");
+    timeSelect.addClass("is-invalid");
+    placeSelect.addClass("is-invalid");
+    return;
+  }
+  if (!date) {
+    dateInput.addClass("is-invalid");
+    error = true;
+  }
+
+  if (time.length === 0) {
+    timeSelect.addClass("is-invalid");
+    error = true;
+  }
+
+  if (place.length === 0) {
+    placeSelect.addClass("is-invalid");
+    error = true;
+  }
+
+  if (error) return;
+
+  let urls = time
+    .map((t) =>
+      place.map(
+        (p) =>
+          `${domain}?module=net_booking&files=booking_place&StepFlag=25&QPid=${p}&QTime=${t}&PT=1&D=${date}`
+      )
+    )
+    .flat();
+
+  $("#active").addClass("disabled");
+  $("#activeTitle").removeClass("d-none");
+
+  let checkTime = null;
+
+  checkTime = setInterval(() => {
+    const now = new Date();
+    const min = now.getMinutes();
+    const sec = now.getSeconds();
+    const msec = now.getMilliseconds();
+
+    console.log(`${min}:${sec}:${msec}`);
+    if (min === 00 && sec >= 0 && msec >= 0) {
+      callApi(urls);
+      clearInterval(checkTime);
+    }
+  });
+}
+
+function callApi(urls) {
+  urls.map((url) => window.open(url));
+}
